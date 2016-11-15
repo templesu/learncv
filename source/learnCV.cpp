9a57872a47e20a4dcf93b8e057f484e26cd1dbac
templesu <templesu@gmail.com>
2016-11-16 07:42:42 +0800

    import

---
+++ source/learnCV.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Learn OpenCV" << std::endl;
+	return 1;
+}
